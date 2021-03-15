625b74c9feba436cc8c6a0a147608f1802f3c240
Max Dayton <daytonmaximus@gmail.com>
2021-03-14 23:42:14 -0600

    some login form styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,15 @@ export default function Home() {
 			<Head>
 				<title>Log In</title>
 			</Head>
-			<div className="container mx-auto w-11/12 bg-light-backgroundMain dark:bg-dark-backgroundMain">
+			<div className="container mx-auto w-5/6 bg-light-backgroundMain dark:bg-dark-backgroundMain mt-20">
 				<h1 className="text-4xl text-light-textMain dark:text-dark-textMain">Log In</h1>
-				<div className="bg-light-backgroundSecondary dark:bg-dark-backgroundSecondary rounded-xl shadow hover:shadow-lg p-5 w-5/6 mx-auto">
-
+				<div className="bg-light-backgroundSecondary dark:bg-dark-backgroundSecondary rounded-xl shadow hover:shadow-lg p-5 w-full mx-auto mt-3">
+					<form className="mx-auto w-full flex flex-col">
+						<label className="text-light-textMain dark:text-dark-textMain text-xl">Email</label>
+						<input className="bg-light-backgroundMain dark:bg-dark-backgroundMain shadow-inner rounded-md text-light-textSecondary dark:text-dark-textSecondary text-lg w-11/12 mx-auto px-3 py-1 my-2" placeholder="name@example.com" />
+						<label className="text-light-textMain dark:text-dark-textMain text-xl">Password</label>
+						<input className="bg-light-backgroundMain dark:bg-dark-backgroundMain shadow-inner rounded-md text-light-textSecondary dark:text-dark-textSecondary text-lg w-11/12 mx-auto px-3 py-1 my-2" placeholder="Password" />
+					</form>
 				</div>
 			</div>
 		</Layout>
