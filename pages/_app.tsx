c712e228a7bd18f918bb7ed36b8a505c68a24fc0
Max Dayton <daytonmaximus@gmail.com>
2021-04-06 20:32:27 -0600

    new auth layout wrapper component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,7 +20,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;1,300;1,400&display=swap" rel="stylesheet" />
-        <meta name="theme-color" content={Colors.dark.backgroundMain} />
+        <meta name="theme-color" content={Colors.dark.backgroundSecondary} />
         {/* <link rel="apple-touch-icon" href="/%PUBLIC_URL%/assets/dfGreenLogo192.png" /> */}
         <meta name="apple-mobile-web-app-status-bar" content={Colors.accentMain} />
 			</Head>
@@ -28,7 +28,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Auth.UserContextProvider supabaseClient={supabase}>
         <Component {...pageProps} />
       </Auth.UserContextProvider>
-      
+
       <style global jsx>{`
         :root {
           background-color: ${dark ? Colors.dark.backgroundMain : Colors.light.backgroundMain};
